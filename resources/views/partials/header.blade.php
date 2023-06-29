6a9fc6bd4258985c3f4aea025574b992e28930fc
MarikaDiGirolamo <marikadigirolamo@gmail.com>
2023-06-29 09:18:27 +0200

    End

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,7 @@
 <header>
-    <nav class="navbar bg-body-tertiary">
+    <nav class="navbar">
         <div class="container-fluid">
-            <a class="navbar-brand">Movies</a>
+            <h1 class="navbar-brand"><strong>Movies List</strong></h1>
             <form class="d-flex" role="search">
                 <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
                 <button class="btn btn-outline-danger" type="submit">Search</button>
